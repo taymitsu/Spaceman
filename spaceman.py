50d8f3353918b237ed8a1e27d82c6eefc50d5d09
Tay <tmitsumori96@gmail.com>
2021-10-04 13:20:23 -0700

    functions and main while loop working with CHECK stretch

--- spaceman.py
+++ spaceman.py
@@ -5,18 +5,78 @@ def load_word():
     words_list = f.readlines()
     f.close()
 
-    words_list = words_list[0].split('') 
+    words_list = words_list[0].split(' ') 
     #word = random.choice() 
     secret_word = random.choice(words_list)
     return secret_word
 
+#checks if all the letters of the secret word have been guessed 
+def is_word_guessed(secret_word, letters_guessed):
+    correctlyGuessed = ' '
+
+    for letter in secret_word:
+        if letter in letters_guessed:
+            correctlyGuessed += letter
+        return correctlyGuessed == secret_word
+
+
+#used to get string correctlyGuessed correct letters guessed, underscores for not yet guessed 
 def get_guessed_word(secret_word, letters_guessed):
+    correctlyGuessed = ' '
+    
+    for letter in secret_word:
+        if letter in letters_guessed:
+            correctlyGuessed += letter
+        else: correctlyGuessed += "_"
+    return correctlyGuessed
 
-def is_guess_in_word(user_guess, secret_word):
+#stretch, letter already entered, do not deduct life. 
+def check(secret_word, guess):
+    for letter in secret_word:
+        if letter == guess:
+            return True
+    return False 
 
+#check if the guessed letter is in the secret word 
+def is_guess_in_word(guess, secret_word):
+    #return guess in secret_word
+    if guess in secret_word: 
+        print(f'Correct!')
+        return True
+    else:
+        print(f'{guess} is incorrect. ')
+        return False 
+
+#main block of code that processes game (huge while loop with if)
 def spaceman(secret_word):
+    print("Welcome, Fellow Earthling! Let's Play Spaceman! ")
+
+    print("You will start with 7 lives. Keep them by guessing the letters in the secret word correctly. Lose one life for each letter guessed incorrectly. ")
     letters_guessed = []
     lives = 7
+    running = True
+    while running: 
+        #update, print statements for users. 
+        guess = input('Enter a letter > ')
+        if is_word_guessed(secret_word, letters_guessed):
+            print('WINNER, WINNER, CHICKEN DINNER!!!')
+            running = False 
+        if is_guess_in_word(guess, secret_word) is False:
+            print(get_guessed_word(secret_word, letters_guessed))
+            print(f'You have {lives} lives left! ')
+            print(f'List of attempted letters: {letters_guessed} ')
+            if check(letters_guessed, guess):
+                print(f'ALERT: {guess} has already been attempted.')
+                print('A life has not been removed. Please proceed cautiously')
+                lives += 1
+            lives -= 1
+            if lives == 0:
+                print(f'So close, but so far.. GAME OVER! The correct word was {secret_word.upper()}')
+                #read secret word as UPPER for easier reading
+                running = False
+                break
+        letters_guessed.append(guess)
+        print(' ')
 
 #Start game 
 secret_word = load_word()
@@ -24,19 +84,20 @@ spaceman(secret_word)
 
 """
 CHECK LIST:
-prog accept USER INPUT for letter guesses
-provide accurate/update feedback per round: right, display word progress
-                                            wrong, lives remaining
+✓prog accept USER INPUT for letter guesses
+✓provide accurate/update feedback per round: right, display word progress
+                                            ✓wrong, lives remaining
 ✓7 lives total
-Read duplicate letters in one pass (all 'l', 'e' in challenge)
-WINNER NOTIFICATION 
-LOSER  NOTIFICATION IS ALL LIVES DEPLETED
+✓Read duplicate letters in one pass (all 'l', 'e' in challenge)- IS WORD GUESSED 
+✓WINNER NOTIFICATION 
+✓LOSER  NOTIFICATION IS ALL LIVES DEPLETED
     STRETCH: 
     Play again? yes/no. loop- continue/break?
-    Alert user is they have already attempted a letter, without deducting life. 
+    ✓Alert user is they have already attempted a letter, without deducting life. 
 """
 
 '''
+greeting needs to be outside of while loop, so it doesn't get repeated!!!
 "Welcome, space explorer! Let's play Spaceman"
 "You will start with 7 lives. Keep them by guessing the letters in the secret word correctly. Guess them wrong, and lose a life each time. "
 "Enter a letter > "
