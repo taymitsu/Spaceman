145bbb22f4670af64f312de7512a8d899a9d15f0
Tay <tmitsumori96@gmail.com>
2021-10-04 19:40:43 -0700

    final touches, notes removed

--- spaceman.py
+++ spaceman.py
@@ -31,8 +31,6 @@ def get_guessed_word(secret_word, letters_guessed):
 #def check(secret_word, guess):
     #for letter in secret_word:
         #if letter == guess:
-            #print(f'ALERT: {guess} has already been attempted. ')
-            #print('A life has not been removed. Please proceed cautiously ')
             #return True
     #return False 
 
@@ -62,70 +60,33 @@ def spaceman(secret_word):
             print(f'Correct! ')
             letters_guessed.append(guess)
             print(f'List of attempted letters: {letters_guessed} ')
-            #print(letters_guessed)
         else: 
             lives -= 1
             print(f'{guess} is incorrect. You have {lives} remaining ')
             letters_guessed.append(guess)
             print(f'List of attempted letters: {letters_guessed} ')
-            
         print(get_guessed_word(secret_word, letters_guessed))
         #print(f'List of attempted letters: {letters_guessed} ')
-
-        #if check(letters_guessed, guess):
-            #lives != 1
-            #print(f'ALERT: {guess} has already been attempted. Please proceed cautiously ')
-        #else: 
-
         if is_word_guessed(secret_word, letters_guessed):
             print('WINNER, WINNER, CHICKEN DINNER!!!')
-
         if lives == 0:
             print(f'So close, but so far... GAME OVER! The correct word was {secret_word.upper()}')
             #read secret word as UPPER for easier reading
             running = False
             break
-        #letters_guessed.append(guess)
-        print(' ')
+#Start game 
+secret_word = load_word()
+spaceman(secret_word)
+
+
+#if check(letters_guessed, guess):
+                #print(f'ALERT: {guess} has already been attempted. ')
+                #lives += 1 
+
+                #letters_guessed.append(guess)
         #play again loop
         #play_again = print("Would you like to play again? (yes/no) > ")
         #if play_again == 'yes':
             #print("Let's play again!")
         #else: 
             #print('Thanks for playing!')
-
-#Start game 
-secret_word = load_word()
-spaceman(secret_word)
-
-"""
-CHECK LIST:
-✓prog accept USER INPUT for letter guesses
-✓provide accurate/update feedback per round: right, display word progress
-                                            ✓wrong, lives remaining
-✓7 lives total
-✓Read duplicate letters in one pass (all 'l', 'e' in challenge)- IS WORD GUESSED 
-✓WINNER NOTIFICATION 
-✓LOSER  NOTIFICATION IS ALL LIVES DEPLETED
-    STRETCH: 
-    Play again? yes/no. loop- continue/break?
-    ✓Alert user is they have already attempted a letter, without deducting life. 
-"""
-
-'''
-greeting needs to be outside of while loop, so it doesn't get repeated!!!
-"Welcome, space explorer! Let's play Spaceman"
-"You will start with 7 lives. Keep them by guessing the letters in the secret word correctly. Guess them wrong, and lose a life each time. "
-"Enter a letter > "
-
-user_guess= a
- IF TRUE 
- correct!
-display word progress: a _ _ _ _
-
-ELSE FALSE
-incorrect ):
-SUBTRACT 1 life, display lives remaining.
-
-LOOP until 7 lives depleted or word guessed 
-'''
